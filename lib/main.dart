6b5ab65c58303c60e75faaf18deecf2df70bfacd
Surajshr <suraj575shrestha@gmail.com>
2021-06-08 20:47:17 +0545

    example of stateFul widget by simply using textField and displaying them on the screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Example",
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.image),
-          title: Text('This is example'),
-        ),
-        body: getListView(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            debugPrint("FAB click");
-          },
-          child: Icon(Icons.add),
-          tooltip: 'Add one more Items',
+  runApp(MaterialApp(
+    title: 'Stateful App Example',
+    home: FavoriteCity(),
+  ));
+}
+
+class FavoriteCity extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _FavoriteCityState();
+  }
+
+}
+
+class _FavoriteCityState extends State<FavoriteCity> {
+  String nameCity = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateful app Example"),
+      ),
+      body: Container(
+        margin:EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onChanged: (String userInput) {
+                setState(() {
+                  nameCity = userInput;
+                });
+
+              },
+            ),
+            Padding(padding:
+            EdgeInsets.all(30.0),
+            child:Text(
+              "Your best City is $nameCity",
+              style: TextStyle(fontSize: 20.0),
+            ))
+          ],
         ),
       ),
-    ),
-  );
-}
-
-void showSnackBar(BuildContext context, String item) {
-  var snackBar = SnackBar(
-    content: Text("You just Tapped $item"),
-    action: SnackBarAction(
-        label: "UNDO",
-        onPressed: () {
-          debugPrint("Performing dummy UNDO operation");
-        }),
-  );
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-}
-
-List<String> getListElements() {
-  var items = List<String>.generate(100, (counter) => "Items $counter");
-  return items;
-}
-
-Widget getListView() {
-  var listItems = getListElements();
-  var listView = ListView.builder(itemBuilder: (context, index) {
-    return ListTile(
-      leading: Icon(Icons.arrow_right),
-      title: Text(listItems[index]),
-      onTap: () {
-        showSnackBar(context, listItems[index]);
-      },
     );
-  });
-  return listView;
+    throw UnimplementedError();
+  }
 }
